27799f4099b4c81760338e7f26d79af9c2849489
TS <ts@tss-macbook-pro.home>
2016-01-10 09:06:18 +0100

    First app commit

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, Directive} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template:`
+        <h1> My Art-Gallery</h1>
+        <h1>...</h1>
+       `,
+       //directives: [MyArt]
+})
+export class AppComponent { }
+
+class MyArt { }
+
+
+https://github.com/ecodraw/tiny-art-gallery
+
+http://coolestguidesontheplanet.com/install-update-latest-version-git-mac-osx-10-9-mavericks/
