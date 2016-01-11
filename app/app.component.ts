324acc9f4de84401338e0368c49c7be24a347897
ecodraw <ecodraw@rosenstadt.ch>
2016-01-11 22:09:55 +0100

    Added click button

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,19 +2,53 @@ import {Component, Directive} from 'angular2/core';
 
 @Component({
     selector: 'art-image',
+    inputs: ['image'],
     template:`
-        <div *ngFor="#name of names"> Value:{{name}}</div>
+        <div *ngFor="#name of names"> Value:{{name}} {{value}}</div>
+        <button (click)="bingo()" class="button">Click </button>
        `
 })
 export class ArtImage { 
     names: string[];
-    
+    image: Image;
+    value: number; 
     
     constructor() {
         this.names = ['Test', 'Bing'];
+        this.image = new Image();
+        this.value = this.image.getCount();
+    }
+    
+    bingo(): void{
+        this.image.plus();
+        this.value = this.image.getCount();
+        //alert("Gugus");
     }
 }
 
+class Image {
+    title: string;
+    link: string;
+    count: number;
+    
+    constructor() {
+        this.title = 'BlueLove';
+        this.link = 'http://www.local.ch';
+        this.count = 10;
+    }
+
+    getCount(): number{
+        return this.count;
+    }
+    
+    plus(): void{
+        this.count +=1;
+    }
+    
+    minus(): void{
+        this.count -=1;
+    }
+}
 
 @Component({
     selector: 'gallery-header',
