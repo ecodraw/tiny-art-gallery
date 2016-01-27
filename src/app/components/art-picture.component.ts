a5fe136703b128d4abfa12e1766b0ee6e4e0e5f2
TS <ts@tss-macbook-pro.home>
2016-01-27 23:02:38 +0100

    playing with scss

--- src/app/components/art-picture.component.ts
+++ src/app/components/art-picture.component.ts
@@ -10,12 +10,12 @@ import {Picture} from '../model/art-model';
     styleUrls: ['app/components/art-picture.component.css'],
     template: `
          <div class="view viewbeta">
-         <img src="../resources/images/preview_default.jpg" />
-         <div class="mask">
-         <h2>Hover {{picture.name}}</h2>
-         <p>A wonderful serenity has taken this days</p>
-         <a href="#" class="info">{{picture.link}}</a>
-         </div>
+            <img src="../resources/images/preview_default.jpg" />
+            <div class="mask">
+                <h2>Hover {{picture.name}}</h2>
+                <p>A wonderful serenity has taken this days</p>
+                <a href="#" class="info">{{picture.link}}</a>
+            </div>
          </div>
        `
 })
--- src/app/components/gallery-app.component.ts
+++ src/app/components/gallery-app.component.ts
@@ -1,9 +1,15 @@
 import {Component, Directive} from 'angular2/core';
+import {GalleryNav} from './gallery-nav.component';
+import {GalleryPage} from './gallery-page.component';
 
 @Component({
     selector: 'gallery-app',
+    styleUrls: ['app/components/gallery-app.component.css'],
     template:`
-        <div>Header</div>
+        <div class="Cmp">
+            <p>Header</p>
+            <gallery-nav>...</gallery-nav>
+        </div>
        `
 })
 export class GalleryApp {}
