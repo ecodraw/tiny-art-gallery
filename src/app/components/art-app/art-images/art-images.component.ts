7ffdd494665beb12c9d1fe2ef44b40a12a93bca3
TS <ts@tss-macbook-pro.home>
2016-01-31 22:35:34 +0100

    added new component

--- src/app/components/art-app/art-images/art-images.component.ts
+++ src/app/components/art-app/art-images/art-images.component.ts
@@ -1,10 +1,11 @@
 import {Component, Directive} from 'angular2/core';
 import {ArtPicture} from '../art-picture/art-picture.component';
+import {ArtImage} from './art-image/art-image.component';
 import {Picture, Collection} from '../../../model/art-model';
 
 @Component({
     selector: 'art-images',
-    directives: [ArtPicture],
+    directives: [ArtImage, ArtPicture],
     inputs: ['collection'],
     host: {
         class: 'images'
@@ -12,6 +13,7 @@ import {Picture, Collection} from '../../../model/art-model';
     styleUrls: ['app/components/art-app/art-images/art-images.component.css'],
     template: `
         <art-picture *ngFor="#picture of collection.pictures"[picture]="picture"></art-picture>
+        <art-image>. img .</art-image>
         <div>Value: {{amount}}</div>
         <br>
         <br>
