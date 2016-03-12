72000896ff69f3a4757929feb7231dae08defb69
ecodraw <ecodraw@rosenstadt.ch>
2016-03-12 22:43:00 +0100

    updated to angular 2 beta 9

--- src/app/boot.ts
+++ src/app/boot.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
 import {bootstrap}    from 'angular2/platform/browser';
 import {provide} from 'angular2/core';
 import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy, RouteConfig} from 'angular2/router';
